5e666002c1ba77c0419e361a4ad80a3d8c7f7cd2
Evan Buss <evan.buss28@gmail.com>
2018-06-21 23:34:42 -0400

    Created new card view for user's books
    Starting to implement a dynamic card view of the user's books. Will make it easier to view book notes, edit notes, and rate the books in a better looking display than a table.

---
+++ src/components/BookCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="card">
+    <header class="card-header">
+      <p class="card-header-title">
+        <slot name="header"></slot>
+      </p>
+      <div class="card-header-icon">
+        <StarRating :rating="rating"></StarRating>
+      </div>
+    </header>
+    <div class="card-content">
+      <div class="content">
+        <slot name="content">
+          Default slot content. Enter custom info using the content slot.
+        </slot>
+      </div>
+    </div>
+    <footer class="card-footer">
+      <a href="#" class="card-footer-item">Rate</a>
+      <a href="#" class="card-footer-item">Edit</a>
+      <a href="#" class="card-footer-item">Delete</a>
+    </footer>
+  </div>
+</template>
+
+<script>
+import StarRating from "./StarRating";
+
+export default {
+  name: "BookCard",
+  components: {
+    StarRating
+  },
+  props: {
+    rating: Number
+  },
+  data: function() {
+    return {};
+  },
+  methods: {
+    debugLog: function(message) {
+      console.log(message);
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+</style>
