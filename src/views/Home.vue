cac1fbdd15b73df6e7198af029f6d8979c208a8d
Evan Buss <evan.buss28@gmail.com>
2018-06-24 00:29:12 -0400

    changed style of home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,26 +9,48 @@
     <!-- Hero-Body -->
     <div class="hero-body">
       <div class="container">
-        <h1 class="title">
+        <h1 class="title is-2">
           Welcome to the Book Keeper
         </h1>
-        <h1 class="title is-2">
-          <span class="icon has-text-dark">
+        <h2 class="title is-spaced is-2">
+          <!-- <span class="icon has-text-dark">
             <i class="fa fa-book"></i>
-          </span>
-        </h1>
+          </span> -->
+            <p>
+              👁
+              <span class="icon has-text-dark">
+                <i class="fa fa-book"></i>
+              </span> 👁 <br>
+              💧👄💧
+            </p>
+        </h2>
+        <h3 class="subtitle is-spaced is-3 has-text-dark">
+          A project by <a class="has-text-light" href="https://github.com/evan-buss/book-list"><em>Evan Buss</em></a>
+        </h3>
+
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import NavBar from '../components/NavBar';
+import NavBar from "../components/NavBar";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     NavBar
   }
-}
+};
 </script>
+
+<style scoped>
+/* a:hover {
+} */
+
+em:hover {
+  text-decoration: line-through;
+
+  color: #00d8ff;
+}
+</style>
